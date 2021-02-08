7a3aa7a6254abf617f945fe00444b87a335eb689
Eduard Aksamitov <e@edaks.co>
2021-02-08 05:49:37 +0300

    test(p-input): focus then blur

--- p-input/PInput.stories.js
+++ p-input/PInput.stories.js
@@ -1,3 +1,5 @@
+import { onMounted, ref } from 'vue'
+
 import PInput from './PInput.vue'
 
 export default {
@@ -19,3 +21,21 @@ export const Default = () => ({
 	components: { PInput },
 	template: '<p-input placeholder="Default Input"/>'
 })
+
+export const Focus = () => ({
+	components: { PInput },
+	template: '<p-input ref="input" placeholder="Default Input"/>',
+	setup () {
+		let input = ref(null)
+
+		onMounted(() => {
+			input.value.focus()
+			setTimeout(() => {
+				input.value.blur()
+			}, 2000)
+		})
+
+		return { input }
+	}
+})
+Focus.storyName = 'Default: Focus then Blur'
