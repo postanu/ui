46234cdd6d9b12af4cc9821f1647263f82b67ec9
Eduard Aksamitov <e@edaks.co>
2021-04-03 20:44:10 +0300

    chore: use tab indents

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,5 +3,5 @@ import { Parameters } from '@storybook/vue3'
 import '../styles/base.styl'
 
 export const parameters: Parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' }
+	actions: { argTypesRegex: '^on[A-Z].*' }
 }
