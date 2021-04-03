dd8385ed0a353a1c3f82f0a17aa1d283220edfb4
Eduard Aksamitov <e@edaks.co>
2021-04-03 23:24:32 +0300

    chore: update exports

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
-import PIcon from './p-icon/PIcon.vue'
-import PLogo from './p-logo/PLogo.vue'
-import PInput from './p-input/PInput.vue'
-import PBadge from './p-badge/PBadge.vue'
-import PAvatar from './p-button/PAvatar.vue'
-import PButton from './p-button/PButton.vue'
-import PTableRow from './p-table-row/PTableRow.vue'
-import PButtonConnect from './p-button/PButtonConnect.vue'
+import PIcon from './components/p-icon/PIcon.vue'
+import PLogo from './components/p-logo/PLogo.vue'
+import PInput from './components/p-input/PInput.vue'
+import PBadge from './components/p-badge/PBadge.vue'
+import PAvatar from './components/p-button/PAvatar.vue'
+import PButton from './components/p-button/PButton.vue'
+import PTableRow from './components/p-table-row/PTableRow.vue'
+import PButtonConnect from './components/p-button/PButtonConnect.vue'
 
-export { icons } from './p-icon/icons.js'
+export { icons } from './components/p-icon/icons.js'
 export {
 	PIcon,
 	PLogo,
