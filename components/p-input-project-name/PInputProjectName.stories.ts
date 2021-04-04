8a27cb41d18766df7be8c2b3594ae3c1bba03ac8
Eduard Aksamitov <e@edaks.co>
2021-04-04 04:15:03 +0300

    feat: init p-input-project-name

---
+++ components/p-input-project-name/PInputProjectName.stories.ts
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/vue3'
+import { defineComponent } from 'vue'
+
+import PInputProjectName from './PInputProjectName.vue'
+
+export default {
+	title: 'PInputProjectName',
+	component: PInputProjectName,
+	argTypes: {
+		create: {
+			action: true,
+			description: 'Triggers on pressing `Enter` and passes the project name.'
+		}
+	}
+} as Meta
+
+export const Default: Story = args => defineComponent({
+	components: { PInputProjectName },
+	setup: () => ({ args }),
+	template: '<p-input-project-name @create="args.create" />'
+})
---
+++ components/p-input-project-name/PInputProjectName.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+p-input.p-h2.p-input-project-name(
+	ref="input"
+	size="25"
+	type="text"
+	name="project-name"
+	autofocus="on"
+	autocorrect="off"
+	autocapitalize="off"
+	placeholder="Type the name of the project"
+	v-model:value="value"
+	@keyup.enter="$emit('create', value)"
+)
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from 'vue'
+
+import PInput from '../p-input/PInput.vue'
+
+export default defineComponent({
+	name: 'PInputProjectName',
+	components: { PInput },
+	emits: ['create'],
+	setup () {
+		let input = ref(null)
+		let value = ref('')
+		let showLabel = computed(() => value.value.length > 1)
+		return {
+			input,
+			value,
+			showLabel
+		}
+	}
+})
+</script>
+
+<style lang="stylus">
+.p-input-project-name
+	font-weight: 700
+	padding: 15px 0 5px 0
+
+.p-input-project-name:focus
+	box-shadow: none
+</style>
