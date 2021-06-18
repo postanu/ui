c72552efbce8e409d45d94da4ddcb0152b1c7201
Eduard Aksamitov <e@edaks.co>
2021-06-18 23:47:13 +0300

    fix: missed `p-pages-table` export

--- index.d.ts
+++ index.d.ts
@@ -8,9 +8,9 @@ import PButton from './components/p-button/PButton.vue'
 import PHeading from './components/p-heading/PHeading.vue'
 import PTableRow from './components/p-table-row/PTableRow.vue'
 import PTableGroup from './components/p-table-group/PTableGroup.vue'
+import PPagesTable from './components/p-pages-table/PPagesTable.vue'
 import PButtonRemove from './components/p-button-remove/PButtonRemove.vue'
 import PQueueHeading from './components/p-queue-heading/PQueueHeading.vue'
-import PSettingsPages from './components/p-settings-pages/PSettingsPages.vue'
 import PButtonConnect from './components/p-button-connect/PButtonConnect.vue'
 import PInputProjectName from './components/p-input-project-name/PInputProjectName.vue'
 
@@ -26,9 +26,9 @@ export {
 	PHeading,
 	PTableRow,
 	PTableGroup,
+	PPagesTable,
 	PButtonRemove,
 	PQueueHeading,
-	PSettingsPages,
 	PButtonConnect,
 	PInputProjectName
 }
