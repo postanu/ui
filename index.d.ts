a7c83b72f1e1cc6b0fc6683a7c5031a55f612d33
Eduard Aksamitov <e@edaks.co>
2021-07-05 00:48:32 +0300

    chore: add exports for new components

--- index.d.ts
+++ index.d.ts
@@ -3,15 +3,19 @@ import PLogo from './components/p-logo/PLogo.vue'
 import PPage from './components/p-page/PPage.vue'
 import PInput from './components/p-input/PInput.vue'
 import PBadge from './components/p-badge/PBadge.vue'
+import PClock from './components/p-clock/PClock.vue'
 import PAvatar from './components/p-avatar/PAvatar.vue'
 import PButton from './components/p-button/PButton.vue'
 import PHeading from './components/p-heading/PHeading.vue'
 import PTableRow from './components/p-table-row/PTableRow.vue'
 import PTableGroup from './components/p-table-group/PTableGroup.vue'
 import PPagesTable from './components/p-pages-table/PPagesTable.vue'
+import PRadioGroup from './components/p-radio-button/PRadioGroup.vue'
+import PRadioButton from './components/p-radio-button/PRadioButton.vue'
 import PButtonRemove from './components/p-button-remove/PButtonRemove.vue'
 import PQueueHeading from './components/p-queue-heading/PQueueHeading.vue'
 import PButtonConnect from './components/p-button-connect/PButtonConnect.vue'
+import PTableHeadline from './components/p-table-headline/PTableHeadline.vue'
 import PInputProjectName from './components/p-input-project-name/PInputProjectName.vue'
 
 export { icons } from './icons/index.js'
@@ -21,14 +25,18 @@ export {
 	PPage,
 	PInput,
 	PBadge,
+	PClock,
 	PAvatar,
 	PButton,
 	PHeading,
 	PTableRow,
 	PTableGroup,
 	PPagesTable,
+	PRadioGroup,
+	PRadioButton,
 	PButtonRemove,
 	PQueueHeading,
 	PButtonConnect,
+	PTableHeadline,
 	PInputProjectName
 }
