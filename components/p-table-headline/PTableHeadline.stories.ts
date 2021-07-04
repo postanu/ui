3273e12f6405013da68f13e800201f5888f255f1
Eduard Aksamitov <e@edaks.co>
2021-07-04 19:08:14 +0300

    feat: `p-table-headline`

---
+++ components/p-table-headline/PTableHeadline.stories.ts
@@ -0,0 +1,31 @@
+import { defineComponent } from 'vue'
+import { Story, Meta } from '@storybook/vue3'
+
+import { ALLOWED_TAGS } from '../p-heading/allowed-tags'
+import PTableHeadline from './PTableHeadline.vue'
+
+export default {
+	title: 'Table/PTableHeadline',
+	component: PTableHeadline,
+	argTypes: {
+		default: {
+			control: 'text'
+		},
+		tag: {
+			control: 'select',
+			options: ALLOWED_TAGS
+		}
+	}
+} as Meta
+
+export const Default: Story = args => defineComponent({
+	components: { PTableHeadline },
+	setup: () => ({ args }),
+	template: `
+		<p-table-headline :tag="args.tag">{{ args.default }}</p-table-headline>
+	`
+})
+Default.args = {
+	default: 'System',
+	tag: 'h3'
+}
---
+++ components/p-table-headline/PTableHeadline.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+p-heading.p-table-headline(:tag="tag" :headline="true")
+	slot
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import { ALLOWED_TAGS } from '../p-heading/allowed-tags'
+import PHeading from '../p-heading/PHeading.vue'
+
+export default defineComponent({
+	name: 'PTableHeadline',
+	components: { PHeading },
+	props: {
+		tag: {
+			type: String,
+			required: true,
+			validator: (value: string) => {
+				return ALLOWED_TAGS.includes(value)
+			}
+		}
+	}
+})
+</script>
+
+<style lang="stylus">
+.p-table-headline
+	line-height: 50px
+	margin-left: 280px
+</style>
