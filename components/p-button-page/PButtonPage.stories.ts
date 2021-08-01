4fd492b23868ed01fef62ec12b2b5fe9380ea1bb
Eduard Aksamitov <e@edaks.co>
2021-08-01 03:54:09 +0300

    feat: `p-button-page`

---
+++ components/p-button-page/PButtonPage.stories.ts
@@ -0,0 +1,42 @@
+import { defineComponent } from 'vue'
+import { Story, Meta } from '@storybook/vue3'
+
+import PPage from '../p-page/PPage.vue'
+import PButtonPage from './PButtonPage.vue'
+
+export default {
+	title: 'PButton/PButtonPage',
+	component: PButtonPage,
+	argTypes: {
+		tag: {
+			control: 'select',
+			options: ['button', 'a']
+		}
+	}
+} as Meta
+
+const Template: Story = args => defineComponent({
+	components: { PButtonPage, PPage },
+	setup: () => ({ args }),
+	template: `
+		<p-button-page v-bind="args">
+			<p-page
+				icon="facebook"
+				avatar="mm.jpg"
+				fullname="Marilyn Manson"
+				username="marilynmanson"
+			/>
+		</p-button-page>
+	`
+})
+
+export const Default = Template.bind({})
+Default.args = {
+	tag: 'button'
+}
+
+export const HideIcon = Template.bind({})
+HideIcon.args = {
+	tag: 'button',
+	hideIcon: true
+}
---
+++ components/p-button-page/PButtonPage.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+component.p-button-page(
+	:is="tag"
+	:class="{ 'p-button-page--hide-icon': hideIcon }"
+)
+	slot
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'PButtonPage',
+	props: {
+		tag: {
+			type: String,
+			default: 'button',
+			validate: (tag: string): boolean => {
+				return ['button', 'a'].includes(tag)
+			}
+		},
+		hideIcon: {
+			type: Boolean,
+			default: false
+		}
+	}
+})
+</script>
+
+<style lang="stylus">
+.p-button-page
+	padding: 10px
+	border-radius: 7.5px
+	cursor: pointer
+	transition: background 0.05s ease-in
+	user-select: none
+
+	&:hover
+		background: var(--p-color-white-01)
+
+	.p-page
+		cursor: pointer
+
+	&:focus
+		box-shadow: none
+		background: var(--p-color-white-01)
+
+		.p-page .p-icon
+			opacity: 1
+
+a.p-button-page
+	display: inline-flex
+
+	&:hover
+		color: var(--p-color-white-09)
+
+.p-button-page--hide-icon
+	.p-page .p-icon
+		opacity: 0
+		transition: opacity 0.05s ease-in
+
+	&:hover
+		.p-page .p-icon
+			opacity: 1
+</style>
