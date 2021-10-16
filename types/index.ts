21332b8f87550c7e5a80fa20c7226bb8c209b512
Eduard Aksamitov <e@edaks.co>
2021-10-16 15:19:01 +0300

    chore: move Vue-specific types to `@postanu/types'

--- types/index.ts
+++
@@ -1,7 +0,0 @@
-import type { ComputedRef, DeepReadonly, Ref } from 'vue'
-
-export type Refable<T> = Ref<T> | ComputedRef<T>
-
-export type MaybeRef<T> = T | Refable<T>
-
-export type MaybeReadonly<T> = T | DeepReadonly<T>
