f818659fea97a03eb987e17c38127337768d0307
Eduard Aksamitov <e@edaks.co>
2021-10-17 15:25:41 +0300

    chore: cleanup exports

--- index.d.ts
+++ index.d.ts
@@ -1,48 +1,50 @@
 import PIcon from './components/p-icon/PIcon.vue'
 import PLogo from './components/p-logo/PLogo.vue'
-import PPage from './components/p-page/PPage.vue'
-import PInput from './components/p-input/PInput.vue'
 import PBadge from './components/p-badge/PBadge.vue'
 import PClock from './components/p-clock/PClock.vue'
 import PAvatar from './components/p-avatar/PAvatar.vue'
-import PButton from './components/p-button/PButton.vue'
 import PHeading from './components/p-heading/PHeading.vue'
-import PTableRow from './components/p-table-row/PTableRow.vue'
-import PTableGroup from './components/p-table-group/PTableGroup.vue'
-import PPagesTable from './components/p-pages-table/PPagesTable.vue'
 import PAttachment from './components/p-attachment/PAttachment.vue'
+import PInput from './components/p-input/PInput.vue'
+import PInputProjectName from './components/p-input-project-name/PInputProjectName.vue'
+import PPage from './components/p-page/PPage.vue'
+import PPagesTable from './components/p-pages-table/PPagesTable.vue'
 import PRadioGroup from './components/p-radio/PRadioGroup.vue'
 import PRadioButton from './components/p-radio/PRadioButton.vue'
+import PButton from './components/p-button/PButton.vue'
 import PButtonRemove from './components/p-button-remove/PButtonRemove.vue'
-import PQueueHeading from './components/p-queue-heading/PQueueHeading.vue'
 import PButtonConnect from './components/p-button-connect/PButtonConnect.vue'
+import PTableRow from './components/p-table-row/PTableRow.vue'
+import PTableGroup from './components/p-table-group/PTableGroup.vue'
 import PTableHeadline from './components/p-table-headline/PTableHeadline.vue'
+import PQueueItem from './components/p-queue-item/PQueueItem.vue'
+import PQueueHeading from './components/p-queue-heading/PQueueHeading.vue'
 import PQueueItemPages from './components/p-queue-item-pages/PQueueItemPages.vue'
-import PInputProjectName from './components/p-input-project-name/PInputProjectName.vue'
 import PQueueItemAttachments from './components/p-queue-item-attachments/PQueueItemAttachments.vue'
 
 export { icons } from './icons/index.js'
 export {
 	PIcon,
 	PLogo,
-	PPage,
-	PInput,
 	PBadge,
 	PClock,
 	PAvatar,
-	PButton,
 	PHeading,
-	PTableRow,
-	PTableGroup,
-	PPagesTable,
 	PAttachment,
+	PInput,
+	PInputProjectName,
+	PPage,
+	PPagesTable,
 	PRadioGroup,
 	PRadioButton,
+	PButton,
 	PButtonRemove,
-	PQueueHeading,
 	PButtonConnect,
+	PTableRow,
+	PTableGroup,
 	PTableHeadline,
+	PQueueItem,
+	PQueueHeading,
 	PQueueItemPages,
-	PInputProjectName,
 	PQueueItemAttachments
 }
