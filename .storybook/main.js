209091f867a79e6587ccde051e6c3ec16740dbc6
Eduard Aksamitov <e@edaks.co>
2021-11-20 23:05:53 +0300

    build: pre-bundle `@postanu/ui` dependencies for development

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,6 +14,9 @@ module.exports = {
 	core: {
 		builder: 'storybook-builder-vite'
 	},
+	/**
+	 * @param config {import('vite').UserConfig}
+	 */
 	async viteFinal (config) {
 		// https://github.com/eirslett/storybook-builder-vite/pull/92
 		// https://github.com/eirslett/storybook-builder-vite/issues/55
@@ -25,6 +28,16 @@ module.exports = {
 		if (config.server) {
 			config.server.fs.allow = [searchForWorkspaceRoot(process.cwd())]
 		}
+
+		// pre-bundle @postanu/ui dependencies to speed up development
+		config.optimizeDeps.include.push(
+			'@postanu/twitter-text',
+			'@postanu/twitter-text/regexp',
+			'hashtag-regex',
+			'vuedraggable',
+			'date-fns',
+			'nanoid'
+		)
 
 		return config
 	}
