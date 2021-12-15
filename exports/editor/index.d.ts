071f2cde3ba4e611378ed1d02bde5e446d878f7e
Eduard Aksamitov <e@edaks.co>
2021-12-15 03:25:26 +0300

    build: split exports

---
+++ exports/editor/index.d.ts
@@ -0,0 +1,3 @@
+export { default as PEditorText } from '../../components/p-editor-text/PEditorText.vue'
+export { default as PEditorPages } from '../../components/p-editor-pages/PEditorPages.vue'
+export { default as PEditorAttachments } from '../../components/p-editor-attachments/PEditorAttachments.vue'
---
+++ exports/queue/index.js
@@ -0,0 +1,4 @@
+export { default as PQueueItem } from '../../components/p-queue-item/PQueueItem.vue'
+export { default as PQueueHeading } from '../../components/p-queue-heading/PQueueHeading.vue'
+export { default as PQueueItemPages } from '../../components/p-queue-item-pages/PQueueItemPages.vue'
+export { default as PQueueItemAttachments } from '../../components/p-queue-item-attachments/PQueueItemAttachments.vue'
