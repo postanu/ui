a58f9963bf271a6f22cde38585637bd4013834a7
Eduard Aksamitov <e@edaks.co>
2021-12-15 03:53:46 +0300

    fix(build): types exports

--- exports/editor/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as PEditorText } from '../../components/p-editor-text/PEditorText.vue'
-export { default as PEditorPages } from '../../components/p-editor-pages/PEditorPages.vue'
-export { default as PEditorAttachments } from '../../components/p-editor-attachments/PEditorAttachments.vue'
