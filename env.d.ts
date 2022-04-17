5ff2210f59c2705352ead39280233516f19d6df8
Eduard Aksamitov <e@edaks.co>
2022-04-17 15:45:25 +0300

    workflow: cleanup

--- env.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable unicorn/no-abusive-eslint-disable */
-/* eslint-disable */
-declare module '*.vue' {
-	import type { DefineComponent } from 'vue'
-	let component: DefineComponent<{}, {}, any>
-	export default component
-}
