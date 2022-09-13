2817ac853d26d937f35ae1dc5ab115e8ab0ab159
Eduard Aksamitov <e@euaaaio.ru>
2022-09-14 02:49:10 +0300

    fix: typo in icons exports

--- components/icons/index.d.ts
+++ components/icons/index.d.ts
@@ -3,7 +3,6 @@ export { default as PIconFacebook } from './p-icon-facebook/PIconFacebook.vue'
 export { default as PIconTwitter } from './p-icon-twitter/PIconTwitter.vue'
 export { default as PIconPostanu } from './p-icon-postanu/PIconPostanu.vue'
 export { default as PIconVK } from './p-icon-vk/PIconVK.vue'
-
 export { default as PIconArrowNorthEast } from './p-icon-arrow-north-east/PIconArrowNorthEast.vue'
 export { default as PIconClose } from './p-icon-close/PIconClose.vue'
 export { default as PIconPlus } from './p-icon-plus/PIconPlus.vue'
