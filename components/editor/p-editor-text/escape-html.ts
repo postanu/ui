e7aa288a07df1906941761b2883db102a75b5eae
Eduard Aksamitov <e@euaaaio.ru>
2023-02-11 22:03:51 +0300

    fix: better types

--- components/editor/p-editor-text/escape-html.ts
+++ components/editor/p-editor-text/escape-html.ts
@@ -1,7 +1,7 @@
 // Stolen from
 // https://github.com/twitter/twitter-text/blob/30e2430d90cff3b46393ea54caf511441983c260/js/src/htmlEscape.js#L1
 
-const HTML_ENTITIES = {
+const HTML_ENTITIES: Record<string, string> = {
 	'&': '&amp;',
 	'>': '&gt;',
 	'<': '&lt;',
