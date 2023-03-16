aad5a64c206ef21babcd20040fe862ba6b8842bb
Eduard Aksamitov <e@euaaaio.ru>
2023-03-16 17:41:53 +0300

    chore: cleanup exports & paths

--- exports/editor.js
+++ exports/editor.js
@@ -1,5 +1,6 @@
 export { default as PEditorText } from '../components/editor/p-editor-text/PEditorText.vue'
 export { default as PEditorTime } from '../components/editor/p-editor-time/PEditorTime.vue'
 export { default as PEditorClock } from '../components/editor/p-editor-clock/PEditorClock.vue'
-export { default as PEditorPages } from '../components/editor/p-editor-pages/PEditorPages.vue'
+export { default as PEditorPage } from '../components/editor/p-editor-page/PEditorPage.vue'
+export { default as PEditorPageList } from '../components/editor/p-editor-page-list/PEditorPageList.vue'
 export { default as PEditorAttachments } from '../components/editor/p-editor-attachments/PEditorAttachments.vue'
