15d278d4d7c738eccdfcf0419ad2cd68a406619d
Eduard Aksamitov <e@euaaaio.ru>
2023-04-19 11:27:20 +0300

    workflow: remove js

--- exports/queue.js
+++
@@ -1,4 +0,0 @@
-export { default as PQueueItem } from '../components/queue/p-queue-item/PQueueItem.vue'
-export { default as PQueueHeading } from '../components/queue/p-queue-heading/PQueueHeading.vue'
-export { default as PQueueItemPages } from '../components/queue/p-queue-item-pages/PQueueItemPages.vue'
-export { default as PQueueItemAttachments } from '../components/queue/p-queue-item-attachments/PQueueItemAttachments.vue'
