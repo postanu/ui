d1368d9275b31179bffddc480399a7a1f5418f95
Eduard Aksamitov <e@euaaaio.ru>
2023-09-13 20:12:53 +0300

    fix(p-attachment): move borders out of box

--- src/components/core/p-attachment/PAttachment.vue
+++ src/components/core/p-attachment/PAttachment.vue
@@ -32,11 +32,16 @@ function onLoad (): void {
 	width: 20px
 	height: 20px
 	background-color: var(--p-color-white-01)
-	border: 1px solid var(--p-color-white-02)
-	border-radius: 3px
 
 .p-attachment--loading
 	--p-attachment-loaded: 0
+
+.p-attachment:after
+	position: absolute
+	inset: -1px
+	content: ""
+	border: 1px solid var(--p-color-white-02)
+	border-radius: 3px
 
 .p-attachment__i
 	position: absolute
@@ -44,7 +49,6 @@ function onLoad (): void {
 	box-sizing: content-box
 	width: 100%
 	height: 100%
-	border-radius: 2px
 	opacity: var(--p-attachment-loaded)
 	transition: opacity 0.1s ease-in
 	object-fit: cover
